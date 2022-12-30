df6884acd73a3e35051c61c3a9325e0e9ee12da9
sbj <sbahnj@gmail.com>
2022-12-30 14:39:34 -0800

    first object info

--- app/src/main/java/com/example/pocket_sonnet/NewPoemActivity.java
+++ app/src/main/java/com/example/pocket_sonnet/NewPoemActivity.java
@@ -18,18 +18,6 @@ public class NewPoemActivity extends AppCompatActivity {
 
     Button findImagesButton;
 
-    // Generate two random numbers between 1 - 470,000
-
-    int first_object = 88;
-    int second_object = 33887;
-
-    // Find the objects with IDs matching the generated numbers
-
-    // Display the images associated with those objects
-
-    // Getting the info for the first object
-    String url_string_first_object =
-            "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + first_object;
     final Handler handler = new Handler();
 
 
@@ -40,6 +28,8 @@ public class NewPoemActivity extends AppCompatActivity {
 
         // set the vars
         findImagesButton = findViewById(R.id.FindImagesButton);
+        final String[] info = {""};
+
 
         // set a listener for making the request
         findImagesButton.setOnClickListener(new View.OnClickListener(){
@@ -50,79 +40,74 @@ public class NewPoemActivity extends AppCompatActivity {
             public void onClick(View v) {
 
 
-
-
-
-
-
                     // make a handler for the search
                     handler.post(new Runnable() {
-
-
 
 
                                          @Override
                                          public void run () {
 
-                                         // define the range
-                                         int max = 470000;
-                                         int min = 1;
-                                         int range = max - min + 1;
 
-                                         int rand = (int) (Math.random() * range) + min;
+                                                 // define the range
+                                                 int max = 470000;
+                                                 int min = 1;
+                                                 int range = max - min + 1;
 
-                                         int object_ID = rand;
+                                                 int rand = (int) (Math.random() * range) + min;
 
-                                         Log.d("object ID", String.valueOf(object_ID));
+                                                 int object_ID = rand;
 
-
-                                         // TODO: catch case if the object doesn't have an image
-                                         // TODO: catch case if objectID doesn't exist
-
-                                         String url_string =
-                                                 "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + object_ID;
+                                                 Log.d("object ID", String.valueOf(object_ID));
 
 
-                                         // Find the object matching the random ID
-                                         AsyncHttpClient client = new AsyncHttpClient();
-
-                                         client.get(url_string, new AsyncHttpResponseHandler() {
-                                             @Override
-                                             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
-                                                 String object_info = new String(responseBody);
-
-                                                 Log.d("object info", object_info);
-                                             }
-
-                                             @Override
-                                             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
-                                                 Log.d("failure", "no object found");
-
-                                                 // Click the button again
-                                                 findImagesButton.callOnClick();
-
-                                             }
-                                         });
+                                                 // TODO: catch case if the object doesn't have an image
 
 
-                                     }
+                                                 String url_string =
+                                                         "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + object_ID;
 
+
+                                                 // Find the object matching the random ID
+                                                 AsyncHttpClient client = new AsyncHttpClient();
+
+                                                 client.get(url_string, new AsyncHttpResponseHandler() {
+                                                     @Override
+                                                     public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                                                         String object_info = new String(responseBody);
+                                                         info[0] = object_info;
+
+                                                         Log.d("object info", object_info);
+
+
+
+
+                                                     }
+
+
+
+                                                     @Override
+                                                     public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                                                         Log.d("failure", "no object found");
+
+                                                         // If no object is found, click the button again
+                                                         findImagesButton.callOnClick();
+
+
+
+                                                     }
+                                                 });
+
+
+
+                                         }
 
             }
-
-
                     );
-
-
-
-
 
             }
         });
-
-
 
     }
 
