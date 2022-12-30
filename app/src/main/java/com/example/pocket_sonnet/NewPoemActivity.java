ba2cb218a78ac1cdd5d223b0d6c9456f15f27885
sbj <sbahnj@gmail.com>
2022-12-30 11:16:31 -0800

    generating random IDs

--- app/src/main/java/com/example/pocket_sonnet/NewPoemActivity.java
+++ app/src/main/java/com/example/pocket_sonnet/NewPoemActivity.java
@@ -17,9 +17,19 @@ import cz.msebera.android.httpclient.Header;
 public class NewPoemActivity extends AppCompatActivity {
 
     Button findImagesButton;
-    String url_string =
-            "https://collectionapi.metmuseum.org/public/collection/v1/search?q="
-                    + "cat";
+
+    // Generate two random numbers between 1 - 470,000
+
+    int first_object = 88;
+    int second_object = 33887;
+
+    // Find the objects with IDs matching the generated numbers
+
+    // Display the images associated with those objects
+
+    // Getting the info for the first object
+    String url_string_first_object =
+            "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + first_object;
     final Handler handler = new Handler();
 
 
@@ -40,36 +50,64 @@ public class NewPoemActivity extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                // make a handler for the search
-                handler.post(new Runnable(){
-                    @Override
-                    public void run() {
-
-                        //TODO get some sort of Internet permissions
 
 
 
-                        // Apply the search term to the string
-                        AsyncHttpClient client = new AsyncHttpClient();
 
-                        client.get(url_string, new AsyncHttpResponseHandler() {
-                            @Override
-                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
-                                Log.d("success", "object found!");
-                            }
-
-                            @Override
-                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
-                                Log.d("failure", "no object found");
-
-                            }
-                        });
+                    // make a handler for the search
+                    handler.post(new Runnable() {
 
 
-                    }
-                });
+
+                                     @Override
+                                     public void run() {
+
+                                         // define the range
+                                         int max = 470000;
+                                         int min = 1;
+                                         int range = max - min + 1;
+
+                                         int rand = (int)(Math.random() * range) + min;
+
+                                         int object_ID = rand;
+
+                                         Log.d("object ID", String.valueOf(object_ID));
+
+
+                                         // TODO: catch case if the object doesn't have an image
+                                         // TODO: catch case if objectID doesn't exist
+
+                                         String url_string=
+                                                 "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + object_ID;
+
+
+
+
+                                         // Find the object matching the random ID
+                                         AsyncHttpClient client = new AsyncHttpClient();
+
+                                         client.get(url_string, new AsyncHttpResponseHandler() {
+                                             @Override
+                                             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                                                 String object_info = new String(responseBody);
+
+                                                 Log.d("object info", object_info);
+                                             }
+
+                                             @Override
+                                             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                                                 Log.d("failure", "no object found");
+
+                                             }
+                                         });
+
+
+                                     }
+                                 }
+
+                    );
 
 
 
