0bab0d0a14845963065469e7c1d91f9cbabec9cc
sbj <sbahnj@gmail.com>
2023-01-05 16:07:36 -0800

    going to generate a title

--- app/src/main/java/com/example/pocket_sonnet/MakeTitleActivity.java
+++ app/src/main/java/com/example/pocket_sonnet/MakeTitleActivity.java
@@ -1,4 +1,24 @@
 package com.example.pocket_sonnet;
 
-public class MakeTitleActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MakeTitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_title);
+
+
+
+        // Generate a title using random title from Met API
+
+
+        // Find a random ID and display its title in a TextView
+
+
+    }
+
 }
